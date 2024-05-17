908d84f3a60a686cb51bd9ad7672f0162e60dfe3
jamers188 <123589840+jamers188@users.noreply.github.com>
2024-05-17 10:59:10 +0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -25,7 +25,7 @@ def add_bg_from_url():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://images.unsplash.com/photo-1488415032361-b7e238421f1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80");
+             background-image: url("https://unsplash.com/photos/city-skyline-during-night-time-GLAPpIlEYKQ");
              background-attachment: fixed;
              background-size: cover
          }}
