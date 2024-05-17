863ef72730b76f0967993055856e5cb935107221
jamers188 <123589840+jamers188@users.noreply.github.com>
2024-05-17 10:53:52 +0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,87 +1,123 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import streamlit as st
-import streamlit.components.v1 as stc
-from transformers import pipeline
 import torch
-import torchvision
-import torchaudio
-from PIL import Image
-from textblob import TextBlob
+# import yake
+# from rake_nltk import Rake
+# rake_nltk_var = Rake()
 
 
-st.set_page_config(layout="wide")
-
-image = Image.open('Studio-Project (2).png')
-st.sidebar.image(image)
-
-image = Image.open('Studio-Project (2).png')
-
-st.image(image, caption=' ')
-t=0
-
-@st.experimental_singleton
-def get_models():
-    # it may be necessary for other frameworks to cache the model
-    # seems pytorch keeps an internal state of the conversation
-    model_name = "facebook/blenderbot-400M-distill"
-    tokenizer = BlenderbotTokenizer.from_pretrained(model_name)
-    model = BlenderbotForConditionalGeneration.from_pretrained(model_name)
-    return tokenizer, model
-
-
-if "history" not in st.session_state:
-    st.session_state.history = []
-
-st.title("Talk To The Bot")
-
-
+st.set_page_config(
+    page_title="Khayal-Writing Tool App"
+)
+st.sidebar.success("Select an App")
+#
+# page_bg_img = """
+# <style>
+# [data-testid="stAppViewContainer"] {
+# background-image url("https://images.unsplash.com/photo-1490633874781-1c63cc424610?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80");
+# background-size: cover;
+# }
+# <style>
+# """
+# st.markdown(page_bg_img, unsafe_allow_html= True)
 def add_bg_from_url():
     st.markdown(
          f"""
          <style>
          .stApp {{
-             background-image: url("https://github.com/jamers188/SEARCHGPOTTT/blob/10100d0f3a8298d0be03ffbc1dcd3662b18b30e3/Studio-Project%20(2).png");
+             background-image: url("https://images.unsplash.com/photo-1488415032361-b7e238421f1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80");
              background-attachment: fixed;
              background-size: cover
          }}
+         [data-testid="stHeader"] {{
+         background-color: rgba(0,0,0,0);
+         }}
+         [data-testid="stSidebar"] {{
+         background-image: url("https://images.unsplash.com/photo-1487528742387-d53d4f12488d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1922&q=80");
+         background-size: cover;
+         }}
          </style>
+         
          """,
          unsafe_allow_html=True
      )
 
-add_bg_from_url() 
+add_bg_from_url()
 
-
-
-def generate_answer():
-    tokenizer, model = get_models()
-    user_message = st.session_state.input_text
-    inputs = tokenizer(st.session_state.input_text, return_tensors="pt")
-    result = model.generate(**inputs)
-    message_bot = tokenizer.decode(
-        result[0], skip_special_tokens=True
-    )  # .replace("<s>", "").replace("</s>", "")
-
-    st.session_state.history.append({"message": user_message, "is_user": True})
-    st.session_state.history.append({"message": message_bot, "is_user": False})
- 
-
-
-
-a=st.text_input("", key="input_text", on_change=generate_answer)
-
-
-
-text = str(a)
-f=TextBlob(text).sentiment.polarity
-t+=f
+#primaryColor="#F63366"
+#backgroundColor="#FFFFFF"
+#secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="Serif"
 
 
 
 
+#
+# base="dark"
+# primaryColor="purple"
+#st.title("Khayal- Writing Tool ")
+st.markdown("<h1 style='text-align: center; color: white;'>Khayal- Writing Tool</h1>", unsafe_allow_html=True)
+st.write("This writing tool currently has two applications within it, poem generation and text summarizer."
+         " To summarize text you can use 'Summarizer from the sidebar. ")
+st.header("Poem Generation")
+st.markdown("This is a poem generator app. you can use it to create poems by providing text to it.")
+
+@st.cache(allow_output_mutation=True)
+def get_model():
+    tokenizer = GPT2Tokenizer.from_pretrained("Silvers-145/khayal-generate")
+    model = GPT2LMHeadModel.from_pretrained("Silvers-145/khayal-generate")
+    return tokenizer, model
+
+tokenizer,model = get_model()
+
+# # load the model
+# #model = GPT2LMHeadModel.from_pretrained("Silvers-145/khayal-generate")
+# #tokenizer = GPT2Tokenizer.from_pretrained("Silvers-145/khayal-generate")
+
+#model.eval()
+
+#prompt = input("Enter the start of poem here:")
+#button = st.button("Create")
+
+prompt = st.text_input("Enter Text:",placeholder="Enter The text here")
+click = st.button("Create")
+if click and prompt:
+    generated = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0)
+
+    sample_outputs = model.generate(
+        generated,
+        do_sample=True,
+        top_k=50,
+        max_length=300,
+        top_p=0.95,
+        num_return_sequences=3
+    )
+
+    for i, sample_output in enumerate(sample_outputs):
+        poemm = tokenizer.decode(sample_output, skip_special_tokens=True)
+    # print("{}: {}\n\n".format(i,
+
+
+    st.text_area("Generated Poem:", poemm,height=250)
+ #   st.snow()
+    st.success("Poem Generated")
 
 
 
+# kw_extractor = yake.KeywordExtractor()
+# text = poemm
+# language = "en"
+# max_ngram_size = 3
+# deduplication_threshold = 0.9
+# numOfKeywords = 5
+# custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
+# keywords = custom_kw_extractor.extract_keywords(text)
+# for kw in keywords:
+#     print(kw)
 
-for chat in st.session_state.history:
-    st_message(**chat)  # unpacking
-st.write(t)
+# from rake_nltk import Rake
+# rake_nltk_var = Rake()
+# rake_nltk_var.extract_keywords_from_text(poemm)
+# kw = rake_nltk_var.get_ranked_phrases()
+# st.t
